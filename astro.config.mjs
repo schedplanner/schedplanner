7377e656e82ef5300e19e63a11b7e7a058f209e0
Bartłomiej Rudecki <przekichane@icloud.com>
2024-09-03 11:49:27 +0200

    feat: setup node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
