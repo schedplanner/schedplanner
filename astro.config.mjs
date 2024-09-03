5de07cd55576cfb60bc29e5a156f59f916b2404f
Bartłomiej Rudecki <przekichane@icloud.com>
2024-09-03 14:27:59 +0200

    feat: setup paraglide for i18n
    * for now use `setLanguageTag()` on each page - to be fixed
    * add polish translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import paraglide from "@inlang/paraglide-astro";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  integrations: [tailwind(), paraglide({ project: "./schedplanner.inlang", outdir: "./src/paraglide" })],
 
   adapter: node({
     mode: "standalone",
   }),
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "pl"],
+  },
 });
