7c441477436bc0548ab19ef0332c12adc698bac4
Bartłomiej Rudecki <przekichane@icloud.com>
2024-09-03 07:50:30 +0200

    feat: add cookies notice

---
+++ src/pages/api/cookies.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ cookies, request, redirect }) => {
+  cookies.set("cookies_accepted", "true", {
+    httpOnly: true,
+    sameSite: "strict",
+    path: "/",
+  });
+
+  return redirect(request.headers.get("referer"));
+};
