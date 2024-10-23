1d608dba16a9826a79ae3c6a7ba5a65be46eb191
Bartłomiej Rudecki <przekichane@icloud.com>
2024-10-23 21:22:11 +0200

    fix(drizzle.config): update schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/lib/database/schema/*.ts",
+  schema: "./src/lib/database/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
