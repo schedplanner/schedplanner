8fc72cdf7044d2ff426ac319209ee00161e0c467
Bartłomiej Rudecki <przekichane@icloud.com>
2024-12-27 14:22:59 +0100

    feat(schedule/Table): multi-select

--- src/pages/api/schedule/index.ts
+++ src/pages/api/schedule/index.ts
@@ -1,10 +1,10 @@
 import { db } from "@lib/database";
-import { schedule } from "@lib/database/schema";
+import { schedule, shift } from "@lib/database/schema";
 
 import type { APIRoute } from "astro";
-import { and, eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 
-export const GET: APIRoute = async () => {
+export const GET: APIRoute = async (request) => {
   const schedules = await db.select().from(schedule);
 
   return new Response(JSON.stringify(schedules), { headers: { "Content-Type": "application/json" } });
@@ -13,28 +13,38 @@ export const GET: APIRoute = async () => {
 export const POST: APIRoute = async ({ request, redirect }) => {
   const form_data = await request.formData();
 
-  const shiftId = form_data.get("shift_id") as unknown as number;
-  const employeeId = form_data.get("employee_id") as unknown as number;
-  const year = form_data.get("year") as unknown as number;
-  const month = form_data.get("month") as unknown as number;
-  const day = form_data.get("day") as unknown as number;
+  const shiftId = form_data.get("shift_id")!.toString().split(",").map(Number);
+  const employeeId = form_data.get("employee_id")!.toString().split(",").map(Number);
+  const year = Number(form_data.get("year"));
+  const month = Number(form_data.get("month"));
+  const day = form_data.get("day")!.toString().split(",").map(Number);
 
-  if (isNaN(shiftId)) {
+  if (shiftId.length === 1 && shiftId[0] === -1) {
     await db
       .delete(schedule)
-      .where(and(eq(schedule.employeeId, employeeId), eq(schedule.year, year), eq(schedule.month, month), eq(schedule.day, day)));
+      .where(and(inArray(schedule.employeeId, employeeId), eq(schedule.year, year), eq(schedule.month, month), inArray(schedule.day, day)));
   } else {
     await db
       .delete(schedule)
-      .where(and(eq(schedule.employeeId, employeeId), eq(schedule.year, year), eq(schedule.month, month), eq(schedule.day, day)));
+      .where(and(inArray(schedule.employeeId, employeeId), eq(schedule.year, year), eq(schedule.month, month), inArray(schedule.day, day)));
 
-    await db.insert(schedule).values({
-      shiftId: shiftId,
-      employeeId: employeeId,
-      year: year,
-      month: month,
-      day: day,
-    });
+    const insertData: (typeof schedule.$inferInsert)[] = [];
+
+    for (const shift of shiftId) {
+      for (const employee of employeeId) {
+        for (const d of day) {
+          insertData.push({
+            shiftId: shift,
+            employeeId: employee,
+            year: year,
+            month: month,
+            day: d,
+          });
+        }
+      }
+    }
+
+    await db.insert(schedule).values(insertData);
   }
 
   return redirect("/");
