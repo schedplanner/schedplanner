3ce5391fee62e2834353a2878d86aa0da5597696
Bartłomiej Rudecki <przekichane@icloud.com>
2024-12-27 14:24:41 +0100

    feat: run dev server in https

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import paraglide from "@inlang/paraglide-astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 import icon from "astro-icon";
 
@@ -17,5 +18,12 @@ export default defineConfig({
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pl"],
+  },
+
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: process.env.DEV,
+    },
   },
 });
