93525370f5882503fd59cf1098b6fda2ef8cc231
Bartłomiej Rudecki <przekichane@icloud.com>
2024-12-27 14:24:31 +0100

    chore(utils): remove unused DefaultDict class

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,18 +4,6 @@ export const fmtShift = (date_string: string): string => {
   return `${hours}:${minutes}`;
 };
 
-export class DefaultDict {
-  constructor(defaultValueFn) {
-    const handler = {
-      get: (obj, prop) => {
-        if (!(prop in obj)) obj[prop] = defaultValueFn();
-        return obj[prop];
-      },
-    };
-    return new Proxy({}, handler);
-  }
-}
-
 export const getRandomId = (prefix?: string): string => {
   const id = Math.random().toString(16).slice(2);
   return prefix ? `${prefix}_` + id : id;
