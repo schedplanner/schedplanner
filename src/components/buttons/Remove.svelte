a2b75c784441038f9762779aa9b1664f81721a83
Bartłomiej Rudecki <przekichane@icloud.com>
2025-01-01 18:53:27 +0100

    feat: rewrite some of the components in svelte

---
+++ src/components/buttons/Remove.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import * as m from "@paraglide/messages";
+
+  interface Props {
+    class?: string;
+  }
+
+  const props: Props = $props();
+</script>
+
+<button class={["bg-red-500 text-white text-sm px-2 py-1 rounded-xl hover:bg-opacity-75 transition-colors", props.class]}>{m.remove()}</button>
---
+++ src/components/form/HTMLInput.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { getRandomId } from "@lib/utils";
+
+  import type { ChangeEventHandler, HTMLInputTypeAttribute } from "svelte/elements";
+
+  interface Props {
+    name: string;
+    type: HTMLInputTypeAttribute;
+    placeholder?: string;
+    label?: string;
+    required?: boolean;
+    class?: string;
+    onchange?: ChangeEventHandler<HTMLInputElement>;
+    value?: string;
+    autocomplete?: AutoFill;
+    disabled?: boolean;
+  }
+
+  const props: Props = $props();
+
+  const id = getRandomId("input");
+</script>
+
+<div class={props.class}>
+  <label for={id} class="text-gray-700">{props.label}</label>
+  <input
+    {id}
+    name={props.name}
+    placeholder={props.placeholder}
+    type={props.type}
+    required={props.required}
+    onchange={props.onchange}
+    value={props.value}
+    autocomplete={props.autocomplete}
+    disabled={props.disabled}
+    class="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring disabled:opacity-75"
+  />
+</div>
